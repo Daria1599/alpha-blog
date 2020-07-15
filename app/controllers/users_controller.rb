f052c34461e7be98e85aa882f1f88125c1777637
Daria <startceva.daria@gmail.com>
2020-07-15 14:55:07 +0300

    User update information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    before_action :set_users, only: [:show, :edit, :update, :destroy]
+
     def new
         @user = User.new
     end
@@ -13,11 +16,27 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @user.update(set_params)
+            flash[:notice] = 'User data was updated successfully.'
+            redirect_to articles_path
+        else
+            render 'edit'
+        end   
+    end
+
     private
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_users
+        @user = User.find(params[:id])
+    end  
 
 
 end
