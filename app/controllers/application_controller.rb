5af4d0ebc791a41178bed824d5570eae8f1c2f7b
Daria <startceva.daria@gmail.com>
2020-07-24 18:41:03 +0300

    Helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] # метод будет искать пользователя в том случае если он еще не найден ( ||= означает "не равно")
+    end
+
+    def logged_in?
+        !!current_user # уточняет если current_user is true
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id # новый пользователь будет залогинен
             flash[:notice] = "Welcome to Alpha Blog, #{@user.username}"
             redirect_to articles_path
         else
